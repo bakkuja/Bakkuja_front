412b7892b5773d389a5b836ca1de4ad724bd1e7a
KoDongE <61736616+KoDongE@users.noreply.github.com>
2022-06-06 22:16:53 +0900

    feautre/KakaoLogin

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import './App.css';
 import React from 'react';
 import Home from './pages/Home';
 import MainInfo from './pages/MainInfo';
-
+import Auth from './pages/Auth';
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
+  const kakao = window.Kakao;
+
+  useEffect(() => {
+    kakao.init(process.env.REACT_APP_KAKAO_KEY);
+  }, [kakao]);
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/mainInfo" element={<MainInfo />} />
+          <Route path="/oauth/kakao/callback" element={<Auth />} />
         </Routes>
       </BrowserRouter>
     </div>
