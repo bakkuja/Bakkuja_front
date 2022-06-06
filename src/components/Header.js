8de028b7fdcc59277b21b2a249803ad079ead021
KoDongE <61736616+KoDongE@users.noreply.github.com>
2022-06-06 22:57:47 +0900

    devleop branch 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import KakaoLoginOut from './KakaoLoginOut';
 const Header = () => {
   return (
     <div className="Header">
       <div>Logo</div>
+      <div>
+        <KakaoLoginOut />
+      </div>
     </div>
   );
 };
---
+++ src/components/PostItem.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+const PostItem = () => {
+  return (
+    <div>
+      <Button>게시글 작성</Button>
+    </div>
+  );
+};
+
+export default PostItem;
